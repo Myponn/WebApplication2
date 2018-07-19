8c77136c2379a03a0bd000c331b015c1f768ae8d
Myponn-NB <myponnst@gmail.com>
2018-07-20 02:05:39 +0700

    AddEFfromDB
    Add EF from DB

--- WebApplication2/WebApplication2/Models/CardInfo.cs
+++ WebApplication2/WebApplication2/Models/CardInfo.cs
@@ -27,5 +27,22 @@ namespace WebApplication2.Models
                 ExpiryDate = ExpiryDate
             };
         }
+        public override bool Equals(object obj)
+        {
+            CardInfo CardToCompare = obj as CardInfo;
+            if (CardToCompare == null)
+                throw new Exception("Can't compare two different objects to each other");
+
+            return this.CardNumber == CardToCompare.CardNumber &&
+                this.ExpiryDate == CardToCompare.ExpiryDate;
+        }
+
+        public override int GetHashCode()
+        {
+            if (String.IsNullOrEmpty(this.CardNumber))
+                return 0;
+
+            return this.CardNumber.GetHashCode();
+        }
     }
 }
