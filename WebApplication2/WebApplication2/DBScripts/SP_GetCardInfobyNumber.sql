c9b0084c4ab2fef94cf66222b5a0cf53b56e8d9c
Myponn-NB <myponnst@gmail.com>
2018-07-20 02:03:38 +0700

    RemoveUnnessessary
    RemoveUnnessessary file and code

--- WebApplication2/WebApplication2/DBScripts/SP_GetCardInfobyNumber.sql
+++
@@ -1,7 +0,0 @@
-/**************************************************************************************\
--- 20180717 - Nipon - CREATE PROCEDURE ValidateCardInfo
-\**************************************************************************************/
-create procedure GetCardInfobyNumber(@CardNo varchar(16)) AS
-BEGIN
-	select top 1 NameOnCard,Card_Number,Expire_Date from CardTable where Card_Number=@CardNo
-END
